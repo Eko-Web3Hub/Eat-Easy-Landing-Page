ab771cefb0627804d91a19af4adc62ee9db0e76e
morgandara <morganslba4@gmail.com>
2025-03-13 20:25:27 +0100

    Add Eat'Easy link to the landing page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,7 +32,7 @@ const Header = () => {
           preferences.
         </p>
         <div className="flex flex-row gap-10 justify-center lg:justify-start">
-          <Link to="" className="contents">
+          <Link to="https://apps.apple.com/fr/app/eateasy/id6739941450" className="contents">
             <button className="primary shadow-custom w-full lg:w-1/3 flex items-center justify-center gap-2">
               Find Recipes
               <img src={AppleLogo} alt="Apple" className="w-5 h-5 invert" />
